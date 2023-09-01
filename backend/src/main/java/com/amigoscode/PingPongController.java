d36c8dcb9d7c6cdcdeccfde7ca40738738bd4a31
anazj <ahowell9070@gmail.com>
2023-09-01 17:38:19 -0400

    reverted back pt.4

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -16,7 +16,7 @@ public class PingPongController
 	{
 		System.out.println("PONG");
 		count++;
-		return new PingPong("HELLOOOOOOOOOO: "+count);
+		return new PingPong("HELLOOOOOOOOOOO : "+count);
 		
 	}
 }
