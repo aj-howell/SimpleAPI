49d59698c47afaabaefd48d21a762b1cc2aa5e22
anazj <ahowell9070@gmail.com>
2023-09-15 22:07:59 -0400

    new angular environment

---
+++ frontend/angular/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = 
+{
+    api: 
+    {
+        base_url: 'https://customer-api.itsahowell.com',
+        auth_url: 'api/v1/auth/login',
+        customer_url: 'api/v1/customers'
+    }
+};
